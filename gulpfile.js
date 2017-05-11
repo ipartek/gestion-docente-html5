acfb22cb01ea10671b3eeec35ad9c4497f746639
Urko Villanueva <uvillanueva@ipartek.com>
2017-05-11 16:06:08 +0200

    Proyecto web

---
+++ gulpfile.js
@@ -0,0 +1,35 @@
+/**
+ * Created by va00 on 10/05/2017.
+ */
+var gulp = require("gulp");
+var babel = require("gulp-babel");
+var babili = require("gulp-babili");
+var concat = require('gulp-concat');
+var cleanCSS = require('gulp-clean-css');
+var clean = require('gulp-clean');
+gulp.task('clean-js', function () {
+    return gulp.src('dist/js', {read: false})
+        .pipe(clean());
+});
+gulp.task('clean-css', function () {
+    return gulp.src('dist/css', {read: false})
+        .pipe(clean());
+});
+gulp.task('minify-css',['clean-css'], function() {
+    gulp.src(['src/css/normalize.css','src/css/main.css'])
+        .pipe(cleanCSS())
+        .pipe(concat('styles-min.css'))
+        .pipe(gulp.dest('dist/css'));
+});
+gulp.task("compile-js",['clean-js'], function () {
+    return gulp.src(['src/js/vendor/jquery-3.2.1.min.js','src/js/main.js'])
+        .pipe(concat('all-min.js', {newLine: ';'}))
+        .pipe(babel())
+        .pipe(babili({
+            mangle: {
+                keepClassNames: true
+            }
+        }))
+        .pipe(gulp.dest("dist/js"));
+});
+gulp.task("compile",['minify-css',"compile-js"]);
