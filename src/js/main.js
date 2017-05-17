4211eb39adfa4d2ad0c6f5d8d0ca1283894f679d
Urko Villanueva <uvillanueva@ipartek.com>
2017-05-17 08:41:21 +0200

    Boostrap Añadido

--- src/js/main.js
+++ src/js/main.js
@@ -9,144 +9,141 @@ var alumnos = [
     {"codigo":3,"nombre":"jorge","apellidos":"manso rodriguez","dni":"16412750e","email":"xxxxx@xxx.xx","telefono":"+3494"}
 ];
 */
-$.noConflict();
-$(document).ready(function($) {
-    // Code that uses jQuery's $ can follow here.
-    $("#contactForm").on("submit",validarFormularioContacto);
-    $("#listadoAlumnos div a:last-child").click(borrarVarios);
 
-    $("#tablaAlumnos tbody").on("click","td:last-child button:last-child",function(){
-        //alert("has pulsado el boton de borrado");
-        var codigo = $(this).parents("tr").find("input[type=checkbox]").val();
-        //Llamar al REST para Borrar
+
+$("#contactForm").on("submit",validarFormularioContacto);
+$("#listadoAlumnos div a:last-child").click(borrarVarios);
+
+$("#tablaAlumnos tbody").on("click","td:last-child button:last-child",function(){
+    //alert("has pulsado el boton de borrado");
+    var codigo = $(this).parents("tr").find("input[type=checkbox]").val();
+    //Llamar al REST para Borrar
+    //
+    // alert(codigo);
+    //borra la tupla del boton que se ha seleccionado
+    $(this).parents("tr").remove();
+});
+$("#tablaAlumnos tbody").on("click","td:last-child button:first-child",function(){
+    //alert("has pulsado el boton de actualizar");
+    var codigo = $(this).parents("tr").find("input[type=checkbox]").val();
+    //Llamar al REST para el GetById
+    var nombre = $(this).parents("tr").find("td:nth-child(2)").text();
+});
+$("#borrartodos").click(function (event) {
+    //attr ---> cambios de atributos
+    // prop --> propiedades
+    // is ----> validacion booleana
+    if($(this).is(":checked")){
+        $("tbody input[type=checkbox]").prop("checked",true);
         //
-        // alert(codigo);
-        //borra la tupla del boton que se ha seleccionado
-        $(this).parents("tr").remove();
-    });
-    $("#tablaAlumnos tbody").on("click","td:last-child button:first-child",function(){
-        //alert("has pulsado el boton de actualizar");
-        var codigo = $(this).parents("tr").find("input[type=checkbox]").val();
-        //Llamar al REST para el GetById
-        var nombre = $(this).parents("tr").find("td:nth-child(2)").text();
-    });
-    $("#borrartodos").click(function (event) {
-        //attr ---> cambios de atributos
-        // prop --> propiedades
-        // is ----> validacion booleana
-        if($(this).is(":checked")){
-            $("tbody input[type=checkbox]").prop("checked",true);
-            //
-            //checked = checked
-            //selected= selected
-            //
-        }else{
-            $("tbody input[type=checkbox]").prop("checked",false);
-        }
-
-
-    });
-    function borrarVarios() {
-        //recoger los checksboxes marcados
-        $("#tablaAlumnos tbody input:checked").each(function () {
-            var codigo = $(this).val();
-            //Llamar al REST
-            $(this).parents("tr").remove();
-
-
-        });
-        //actualizar el nº de alumnos
-        $("tbody tr").length;
-
-    }
-    function validarFormularioContacto(){
-        //recoger los valores de la vista
-        var pdni = $("#dni").val();
-        var pnombre = $("#nombre").val();
-        var papellidos = $("#apellidos").val();
-        var ptelefono = $("#telefono").val();
-        var valido = false;
-        //evaluarlos
-        var dniValido= validarDni(pdni); //en funcion de si estan bien o mal o se envia o no
-        var nomValido = validarNombre(pnombre);
-        var apeValido = validarApellidos(papellidos);
-        var teleValido = validarTelefono(ptelefono);
-        $("#dni").siblings("div.text-error").text("");
-        $("#nombre").siblings("div.text-error").text("");
-        $("#apellidos").siblings("div.text-error").text("");
-        $("#telefono").siblings("div.text-error").text("");
-        if(dniValido&&nomValido&&apeValido&&teleValido){
-            // $("#contactForm").submit();//se envia el Formulario(Consumir REST)
-            valido = true;
-        }else {
-            //mostar mensaje de error
-            if(!dniValido){
-                $("#dni").siblings("div.text-error").text("El DNI esta mal formado");
-            }
-            if(!nomValido){
-                $("#nombre").siblings("div.text-error").text("El nombre tiene que tener al menos 3 letras");
-            }
-            if(!apeValido){
-                $("#apellidos").siblings("div.text-error").text("Los apellidos tienen que tener al menos 7 letras");
-            }
-            if(!teleValido){
-                $("#telefono").siblings("div.text-error").text("El telefono no es valido, tiene que tener 9 numeros");
-            }
-            //text y html
-        }
-        return false;
-    }
-   // cargarArrayAlumnos();
-    function cargarArrayAlumnos(alumnos) {
-        //recorrer el array
-        if (alumnos.length > 0) {
-            for(var i = 0; i < alumnos.length; i++) {
-                console.log(alumnos[i]);
-                var codigo = alumnos[i].codigo;
-                var nombre = alumnos[i].nombre;
-                var apellidos = alumnos[i].apellidos;
-                var email = alumnos[i].email;
-                var dni = alumnos[i].dni;
-                var htmlEdit ="<button>Editar</button>";
-                var htmlDelete ="<button>Borrar</button>";
-
-                var texto = "<tr><td><input type='checkbox' value='" + codigo + "'></td><td>"+nombre+"</td><td>"+apellidos+"</td><td>"+dni+"</td><td>"+email+"</td><td>"+htmlEdit+htmlDelete+"</td></tr>";
-                //añadir el html correspondiente a la página
-                $("#tablaAlumnos tbody").append(texto);
-                //-->
-            }
-            $("#tablaAlumnos tfoot td").html("<span class='text-error'>Total alumnos:"+alumnos.length,10+"</span>");
-        }else{
-            $("#tablaAlumnos").remove();
-            $("#listadoAlumnos").text("No se han encontrado alumnos")
-        }
-    }
-    const urlAlumnos = "http://localhost:8080/gestiondocente/api/alumnos.json"
-    ajax({"url":urlAlumnos,"method":"get"})
-        .then(function (data) {
-            cargarArrayAlumnos(data);
-            //aqui tengo los datos cargados (data)
-            //console.log(data);
-           /* for(var i =  0; i < data.length; i ++){
-                  var alumno = data[i];
-
-            }*/
-        })
-        .then(function () {
-            //poner mensaje los datos se han cargado correctamente
-        })
-        .catch(function (jqXHR, textStatus, errorThrown ) {
-            console.log(jqXHR);
-            //gestión de errores del primer metodo.
-        });
-    function ajax(opciones) {
-        return new Promise(function (resolve, reject) {
-            $.ajax(opciones).done(resolve).fail(reject);
-        });
+        //checked = checked
+        //selected= selected
+        //
+    }else{
+        $("tbody input[type=checkbox]").prop("checked",false);
     }
 
 
 });
+function borrarVarios() {
+    //recoger los checksboxes marcados
+    $("#tablaAlumnos tbody input:checked").each(function () {
+        var codigo = $(this).val();
+        //Llamar al REST
+        $(this).parents("tr").remove();
+
+
+    });
+    //actualizar el nº de alumnos
+    $("tbody tr").length;
+
+}
+function validarFormularioContacto(){
+    //recoger los valores de la vista
+    var pdni = $("#dni").val();
+    var pnombre = $("#nombre").val();
+    var papellidos = $("#apellidos").val();
+    var ptelefono = $("#telefono").val();
+    var valido = false;
+    //evaluarlos
+    var dniValido= validarDni(pdni); //en funcion de si estan bien o mal o se envia o no
+    var nomValido = validarNombre(pnombre);
+    var apeValido = validarApellidos(papellidos);
+    var teleValido = validarTelefono(ptelefono);
+    $("#dni").siblings("div.text-error").text("");
+    $("#nombre").siblings("div.text-error").text("");
+    $("#apellidos").siblings("div.text-error").text("");
+    $("#telefono").siblings("div.text-error").text("");
+    if(dniValido&&nomValido&&apeValido&&teleValido){
+        // $("#contactForm").submit();//se envia el Formulario(Consumir REST)
+        valido = true;
+    }else {
+        //mostar mensaje de error
+        if(!dniValido){
+            $("#dni").siblings("div.text-error").text("El DNI esta mal formado");
+        }
+        if(!nomValido){
+            $("#nombre").siblings("div.text-error").text("El nombre tiene que tener al menos 3 letras");
+        }
+        if(!apeValido){
+            $("#apellidos").siblings("div.text-error").text("Los apellidos tienen que tener al menos 7 letras");
+        }
+        if(!teleValido){
+            $("#telefono").siblings("div.text-error").text("El telefono no es valido, tiene que tener 9 numeros");
+        }
+        //text y html
+    }
+    return false;
+}
+// cargarArrayAlumnos();
+function cargarArrayAlumnos(alumnos) {
+    //recorrer el array
+    if (alumnos.length > 0) {
+        for(var i = 0; i < alumnos.length; i++) {
+            console.log(alumnos[i]);
+            var codigo = alumnos[i].codigo;
+            var nombre = alumnos[i].nombre;
+            var apellidos = alumnos[i].apellidos;
+            var email = alumnos[i].email;
+            var dni = alumnos[i].dni;
+            var htmlEdit ="<button>Editar</button>";
+            var htmlDelete ="<button>Borrar</button>";
+
+            var texto = "<tr><td><input type='checkbox' value='" + codigo + "'></td><td>"+nombre+"</td><td>"+apellidos+"</td><td>"+dni+"</td><td>"+email+"</td><td>"+htmlEdit+htmlDelete+"</td></tr>";
+            //añadir el html correspondiente a la página
+            $("#tablaAlumnos tbody").append(texto);
+            //-->
+        }
+        $("#tablaAlumnos tfoot td").html("<span class='text-error'>Total alumnos:"+alumnos.length,10+"</span>");
+    }else{
+        $("#tablaAlumnos").remove();
+        $("#listadoAlumnos").text("No se han encontrado alumnos")
+    }
+}
+const urlAlumnos = "http://localhost:8080/gestiondocente/api/alumnos"
+ajax({"url":urlAlumnos,"method":"get"})
+    .then(function (data) {
+        cargarArrayAlumnos(data);
+        //aqui tengo los datos cargados (data)
+        //console.log(data);
+       /* for(var i =  0; i < data.length; i ++){
+              var alumno = data[i];
+
+        }*/
+    })
+    .then(function () {
+        //poner mensaje los datos se han cargado correctamente
+    })
+    .catch(function (jqXHR, textStatus, errorThrown ) {
+        console.log(jqXHR);
+        //gestión de errores del primer metodo.
+    });
+function ajax(opciones) {
+    return new Promise(function (resolve, reject) {
+        $.ajax(opciones).done(resolve).fail(reject);
+    });
+}
+
 function validarNombre(nombre){
     const pattern = new RegExp(/[a-zA-Z]{3,}/);
     return pattern.test(nombre);
